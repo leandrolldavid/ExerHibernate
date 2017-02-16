96d791d21be54e10f83145405a04ac8ced12fab7
leandrolldavid <leandrolldavid@gmail.com>
2017-02-16 21:07:06 -0200

    exerc

---
+++ ExerHibernate1602/src/java/Cliente.java
@@ -0,0 +1,53 @@
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ra21551055
+ */
+@Entity
+public class Cliente {
+    
+    private String nome;
+    private String End;
+    private int idade;
+    @Id
+    private int Ra;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEnd() {
+        return End;
+    }
+    public void setEnd(String End) {
+        this.End = End;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getRa() {
+        return Ra;
+    }
+
+    public void setRa(int Ra) {
+        this.Ra = Ra;
+    }
+    
+}
---
+++ ExerHibernate1602/src/java/SalvarCliante.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author ra21551055
+ */
+public class SalvarCliante extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            Cliente p1 = new Cliente();
+            p1.setNome("Leandro");
+            p1.setIdade(26);
+            p1.setEnd("Rua 17");
+            p1.setRa(21551055);
+            
+            //conectar com o banco
+            Session sessao = HibernateUtil.getSessionFactory().openSession();
+            //criar ponto de restauraçao, aguarda os dados na memoria sem persistir no banco
+            Transaction tx = sessao.beginTransaction();
+            //
+            sessao.save(p1);
+            sessao.flush();
+            
+            tx.commit();
+            sessao.close();
+            
+            
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SalvarCliante</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet SalvarCliante at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
